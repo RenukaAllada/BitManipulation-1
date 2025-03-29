831dfafd36b96573dc078e5b7419760104d23793
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-03-29 18:32:46 -0500

    Completed 2 exercises

--- sample.java
+++ sample.java
@@ -1,6 +1,8 @@
 class sample{
     /***************PROBLEM-1**************/
 
+
+
     /***************PROBLEM-2**************/
 
     //TC:0(n)
@@ -14,6 +16,28 @@ class sample{
             return xor;
         }
     }
-    
+
     /***************PROBLEM-3**************/
+    //TC:0(n)
+//SC:0(1)
+    class Solution {
+        public int[] singleNumber(int[] nums) {
+            if(nums==null || nums.length==0){
+                return new int[]{};
+            }
+            int xor=0;
+            for(int num:nums){
+                xor=xor ^ num;
+            }
+
+            int temp=xor & (-xor);
+            int xor2=0;
+            for(int num:nums){
+                if((temp & num)!=0){
+                    xor2=xor2 ^ num;
+                }
+            }
+            return new int[]{xor2,xor ^ xor2};
+        }
+    }
 }
