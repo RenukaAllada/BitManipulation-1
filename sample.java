b361ce6ed768ca584f1636266b3b73812a946cf8
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-03-29 19:19:54 -0500

    Completed 3 exercises

--- sample.java
+++ sample.java
@@ -1,7 +1,38 @@
 class sample{
     /***************PROBLEM-1**************/
+//TC:0(log(dividend))
+        //SC:0(1)
+    class Solution {
+        public int divide(int dividend, int divisor) {
+            if(dividend!=0 && divisor==0){
+                return Integer.MAX_VALUE;
+            }
+            if(dividend==Integer.MIN_VALUE && divisor==-1){
+                return Integer.MAX_VALUE;
+            }
 
+            long ldivisor=Math.abs((long)divisor);
+            long ldividend= Math.abs((long)dividend);
+            int result=0;
+            while(ldividend>=ldivisor){
+                int shifts=0;
+                while(ldividend >= (ldivisor<<shifts)){
+                    shifts++;
+                }
+                shifts--;
+                result+=(1<<shifts);
+                ldividend=ldividend-(ldivisor<<shifts);
+            }
 
+            if(dividend>0 && divisor<0){
+                return -result;
+            }
+            if(dividend<0 && divisor>0){
+                return -result;
+            }
+            return result;
+        }
+    }
 
     /***************PROBLEM-2**************/
 
